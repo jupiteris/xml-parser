984ec7686a3a0759f834cfa3e51ded38599c60c4
jupiteris <jupiter.programmer@gmail.com>
2020-10-13 23:38:03 +0400

    7th

--- src/Document/QRGenerater.js
+++ src/Document/QRGenerater.js
@@ -7,7 +7,7 @@ export const QRGenerater = ({ url, infos = [], signature }) => {
     <div className="qr">
       <div className="qr-img">
         <QRCode value={url ?? ''} size={180} />
-        <span style={{ marginTop: 10 }}>{signature?.slice(0, 6) ?? ''}</span>
+        <span style={{ marginTop: 10 }}>Cod.Seg.: {signature?.slice(0, 6) ?? ''}</span>
       </div>
       <div className="qr-info">{infos.length ? infos.map((i, index) => <span key={index}>{i}</span>) : ''}</div>
     </div>
